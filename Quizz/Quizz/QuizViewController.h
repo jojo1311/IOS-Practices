13a0beb6961e608f84bf1cf490838f7181fc95b9
jojo1311 <josette@cocuyostudio.com>
2015-01-07 21:10:43 -0500

    created labels and buttons

---
+++ Quizz/Quizz/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  Quizz
+//
+//  Created by Josette Rojas on 1/6/15.
+//  Copyright (c) 2015 cocuyostudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
---
+++ Quizz/Quizz/QuizViewController.m
@@ -0,0 +1,30 @@
+//
+//  QuizViewController.m
+//  Quizz
+//
+//  Created by Josette Rojas on 1/6/15.
+//  Copyright (c) 2015 cocuyostudio. All rights reserved.
+//
+
+#import "QuizViewController.h"
+
+@interface QuizViewController ()
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
+@end
+
+@implementation QuizViewController
+
+- (IBAction)showQuestion:(id)sender
+{
+    
+}
+
+- (IBAction)showAnswer:(id)sender
+{
+    
+}
+
+@end
